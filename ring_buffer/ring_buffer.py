e9de2d6f95289c63314b96508445de76a7a9047a
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-15 02:46:34 -0700

    complete ring buffer with no errors and passing all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity, items=[]):
+        # replacing oldest instead of doing like a queue
+        # forces us to keep track of index of element to replace 
+        self.index = 0
+        self.capacity = capacity
+        self.items = list(items) 
 
     def append(self, item):
-        pass
+        # if full, replace item at index
+        if len(self.items) == self.capacity:
+        	self.items[self.index] = item
+        else:
+        # append when capacity not reached
+        	self.items.append(item)
+        # increment index by one mode length 
+        self.index = (self.index + 1 ) % self.capacity
 
     def get(self):
-        pass
+        return self.items
