65dc5c4ffbd7cb41f33b3feba53df9cd9dbe16be
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-15 03:23:04 -0700

    experiment, using set has 8/1000ths of a second, search is O(1)

--- names/names.py
+++ names/names.py
@@ -16,10 +16,10 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 # The original code has a run time of O(n) * O(n) which is O(n**2)
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 names_1_bst = BSTNode(None)
 
 for name_1 in names_1:
@@ -28,6 +28,8 @@ for name_1 in names_1:
 for name_2 in names_2:
 	if names_1_bst.contains(name_2):
 		duplicates.append(name_2)
+
+# duplicates = list(set(names_1) & set(names_2))
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
